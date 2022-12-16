1540afeb96aa1050a7d869f4355e888519060d09
Marimo Shimomura <mari619s@yahoo.co.jp>
2022-12-16 23:50:12 +0900

    Chapter1

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marimo Shimomura on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
