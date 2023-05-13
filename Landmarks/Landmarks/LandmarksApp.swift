181c26411325e5699a02f5223836c06286327659
nanosuke <82511590+nanosuke@users.noreply.github.com>
2023-05-13 17:21:33 +0900

    Composing Complex Interfaces

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -1,20 +1,11 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Marimo Shimomura on 2022/12/16.
-//
-
 import SwiftUI
+
 
 @main
 struct LandmarksApp: App {
-  @StateObject private var modelData = ModelData() // @StateObject attribute to initialize a model object for a given property only once during the life time of the app
-  
     var body: some Scene {
         WindowGroup {
-            ContentView()
-            .environmentObject(modelData)
+
         }
     }
 }
