e603c3a1fca7ca7849492d89800750890d0dbb22
nanosuke <82511590+nanosuke@users.noreply.github.com>
2024-02-22 16:43:30 +0900

    Building lists and navigation

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable { // Hashable：値に基づいてハッシュ値を計算できるため Codable：型をエンコードしたりデコードしたりできるため Identifiable：一意性を識別するための仕組み
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String // read the name of the image
+    var image: Image{ // loads an image from the asset
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates // the storage in the JSON data
+    var locationCoordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude:coordinates.longitude
+        )
+    }
+
+    struct Coordinates: Hashable, Codable{ // Hashable:その型の値を元にハッシュ値が計算可能である　Codable:任意のデータ型に変換できる
+        var latitude: Double
+        var longitude: Double
+    }
+}
--- Landmarks/Landmarks/Views/MapView.swift
+++ Landmarks/Landmarks/Views/MapView.swift
@@ -2,19 +2,19 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+
     var body: some View {
-        Map(initialPosition: .region(region))
+        Map(position: .constant(.region(region)))
     }
 
     private var region: MKCoordinateRegion{
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 34.68743856037725,
-                longitude: 135.52586595592916),
+            center: coordinate,
             span:  MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     }
 }
 
 #Preview {
-    MapView()
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
 }
